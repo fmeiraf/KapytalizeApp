6cdbcb1f0245498adbcd6d7471d48c30e7c24df8
Fernando Meira <fmeira.filho@gmail.com>
2019-06-08 13:46:59 -0300

    changed debug var to environ var

--- HaluuraApp/settings.py
+++ HaluuraApp/settings.py
@@ -26,8 +26,8 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-#DEBUG = os.environ.get('DEBUG_VALUE')
+#DEBUG = True
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True' )
 
 ALLOWED_HOSTS = ['kapytalize.herokuapp.com']
 
